e7ba85513f204519565a46b6a755b717a4aad929
Shaun Jackson <sjackson95@gmail.com>
2019-10-01 16:51:29 -0700

    abstracted out post list into template along with post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../templates/Layout"
+import PostList from "../templates/PostList"
 import styled from "styled-components"
-import Card from "../components/Card"
-const HeadingTitle = styled.div`
-  display: inline-block;
-  border-bottom: 1px solid;
-`
 
-const BlogWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  width: 100%;
-`
-
-export default ({ data }) => {
+export default () => {
   return (
     <Layout>
-      <BlogWrapper>
-        {data.allMdx.edges.map(({ node }) => (
-          <Card key={node.id} post={node}></Card>
-        ))}
-      </BlogWrapper>
+      <PostList />
     </Layout>
   )
 }
-export const query = graphql`
-  query {
-    allMdx {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-            author
-            featuredImage {
-              childImageSharp {
-                fluid(maxWidth: 140) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-          fields {
-            slug
-          }
-          excerpt(pruneLength: 80)
-        }
-      }
-    }
-  }
-`
---
+++ src/templates/PostList.js
@@ -0,0 +1,51 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+import Card from "../components/Card"
+
+const PostWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+`
+
+export default ({ data }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allMdx {
+          totalCount
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                date(formatString: "DD MMMM, YYYY")
+                author
+                featuredImage {
+                  childImageSharp {
+                    fluid(maxWidth: 140) {
+                      ...GatsbyImageSharpFluid
+                    }
+                  }
+                }
+              }
+              fields {
+                slug
+              }
+              excerpt(pruneLength: 80)
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <PostWrapper>
+        {data.allMdx.edges.map(({ node }) => (
+          <Card key={node.id} post={node}></Card>
+        ))}
+      </PostWrapper>
+    )}
+  />
+)
